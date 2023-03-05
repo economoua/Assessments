2a4dd9e28b53cf4d502f362a5c689d7100f716f6
economoua <74218330+economoua@users.noreply.github.com>
2023-03-05 14:57:34 -0800

    PWAssessment: Initial Commit
    Completed design for an assessment project based on Boston transportation API

---
+++ PWAssessment/main.py
@@ -0,0 +1,54 @@
+# main.py
+from route_finder import RouteFinder
+from stop_finder import StopFinder
+from tkinter import *
+
+#Small little function to clear default input when the user goes to type in the stop search.
+def clear(*args):
+    entry.delete(0, "end")
+
+root = Tk()
+root.title("Travel Finder")
+root.geometry("250x300")
+
+route_finder = RouteFinder(root)
+
+stop_finder = StopFinder(root)
+
+# This is all the set up on the root tkinter to display labels/buttons. I would have prefered to set it up where these buttons refresh the main
+# root rather than opening new frames in new windows, but with time constraints I went with the method I knew.
+ok=Label(root, text="Press a button to see all associated lines:", relief="solid", justify=CENTER)
+ok.grid(row=1,column=1, pady=5, padx=20)
+ok.columnconfigure(1, weight=1)
+
+button_rails = Button(root, text="Standard Rail Routes", command=lambda: route_finder.print_routes("0,1", "All Standard Rails:\n"))
+button_rails.grid(row=2, column=1, pady=3)
+
+button_commuter_rails = Button(root, text="Commuter Rail Routes", command=lambda: route_finder.print_routes("2", "All Commuter Rails:\n"))
+button_commuter_rails.grid(row=3, column=1, pady=3)
+
+button_bus = Button(root, text="Bus Routes", command=lambda: route_finder.print_routes("3", "All Bus Routes:\n"))
+button_bus.grid(row=4, column=1, pady=3)
+
+button_ferry = Button(root, text="Ferry Routes", command=lambda: route_finder.print_routes("4", "All Ferry Routes:\n"))
+button_ferry.grid(row=5, column=1, pady=3)
+
+stop_request=Label(root, text="Enter a standard rail route to see all stops:", relief="solid", justify=CENTER)
+stop_request.grid(row=8,column=1, pady=10)
+
+entryvalue = StringVar()
+text_box_placeholder = "Enter Text: Ex. Red"
+entry = Entry(root, textvariable=entryvalue)
+entry.insert(0, text_box_placeholder)
+entry.grid(row=10, column=1)
+
+entry.bind("<FocusIn>", clear)
+
+search_button = Button(root, text="Search Stops", command=lambda: stop_finder.print_stops(entryvalue, "All routes found on route \"" + entryvalue.get() + "\":\n"))
+search_button.grid(row=11, column=1, pady=3)
+
+
+exit_button = Button(root, text="Exit", command=lambda: root.destroy())
+exit_button.grid(row=13, column=1, pady=10)
+
+root.mainloop()
---
+++ PWAssessment/stop_finder.py
@@ -0,0 +1,78 @@
+import requests
+import json 
+import string
+from tkinter import *
+
+class StopFinder:
+
+    def __init__(self, root):
+        self.root = root
+
+#This function creates a new window to allow the user to either see all stops along a route after validation of input or if input 
+# validation fails it allows the user to try again and refreshes a new window. With more time I would have changed this to 
+# refreshing the same root window to update information rather than closing and opening a window each time. 
+    def print_stops(self, route_value, header):
+
+        top = Toplevel(self.root)
+        top.geometry("450x450")
+        top.title("Route Request")
+
+        scrollbar = Scrollbar(top)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+
+        if self.scan_and_verify_input(route_value.get()) is False:
+            stop_request=Label(top, text=route_value.get() + " was not a valid route. Try again", relief="solid", justify=CENTER)
+            stop_request.pack()
+
+            entryvalue = StringVar()
+
+            entry = Entry(top, textvariable=entryvalue)
+            entry.pack()
+
+            search_button = Button(top, 
+                text="Search Stops", 
+                command=lambda: [self.print_stops(entryvalue, "All routes found on route \"" + entryvalue.get() + "\":\n"), top.destroy()])
+            search_button.pack()
+            exit_button = Button(top, text="Back", command=lambda: top.destroy())
+            exit_button.pack()
+        else: 
+            response_stops = requests.get('https://api-v3.mbta.com/stops?filter[route]='+string.capwords(route_value.get()))
+            stop_data = response_stops.text
+            parsed_json = json.loads(stop_data)
+
+            stop_list = Listbox(top)
+            stop_list.config(width=400, height= 400, yscrollcommand = scrollbar.set)
+            stop_list.insert(END, header)
+
+            id_string = 'ID: '
+            name_string = '  - NAME: '
+
+            for each in parsed_json['data']:
+                stop_list.insert(END, "\n")
+                stop_list.insert(END, id_string + each['id'] + name_string + each['attributes']['name'])
+                stop_list.insert(END, "Address: " + each['attributes']['address'])
+
+            exit_button = Button(top, text="Back", command=lambda: top.destroy())
+            exit_button.pack()
+            stop_list.pack(side=LEFT, fill=BOTH)
+            scrollbar.config(command = stop_list.yview)
+
+#This function is for validation to verify the user entered an expected value in the 
+# list of existing standard rail routes. Example: Red, Blue, Orange
+# Currently not able to allow a user to search by the long name of a rail such as "Red Line", 
+# though functionality would be possible to add through string transformations and another filter criteria.
+# I would also like to make this more robust to take in comma seperated values to show more than one route at a time. 
+    def scan_and_verify_input(self, user_entry):
+        all_routes = requests.get('https://api-v3.mbta.com/routes?filter[type]=0,1')
+        parsed_json = json.loads(all_routes.text)
+
+        route_list = []
+
+        for each in parsed_json['data']:
+            route_list.append(each['id'])
+
+        if string.capwords(user_entry) not in route_list:
+            return False
+        else:
+            return True
